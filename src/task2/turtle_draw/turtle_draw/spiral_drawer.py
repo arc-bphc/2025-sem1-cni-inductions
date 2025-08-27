d46c1ff83d28e38da99620e2fb62760f9881b347
Abhay Aysola <abhay.ayyasola@gmail.com>
2025-08-27 18:05:32 +0530

    task2: circle drawer + spiral drawer with point generation - other shapes can be added

--- src/task2/turtle_draw/turtle_draw/spiral_drawer.py
+++ src/task2/turtle_draw/turtle_draw/spiral_drawer.py
@@ -3,20 +3,24 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from math import pi, sin, cos, floor
 
-class CircleTurtleNode(Node):
+class SpiralTurtleNode(Node):
     def __init__(self):
-        super().__init__('circle_turtle_node')
+        super().__init__('spiral_turtle_node')
 
         self.frequency = 0.1
 
-        # circle parameters
-        self.circle_radius = 2
+        # spiral parameters
+        self.spiral_radius = 0.01
 
         # initializing points list
         self.points = []
-        for theta in range(0, 2*floor(pi*10)+1):
+        i = 0
+        self.points_length = 6*floor(pi*10)+1
+        for theta in range(0, self.points_length):
             theta /= 10
-            self.points.append((self.circle_radius*cos(theta), self.circle_radius*sin(theta)))
+            radius = self.spiral_radius*(self.points_length - i)
+            self.points.append((radius*cos(theta), radius*sin(theta)))
+            i += 1
         self.cur_point_index = 0
 
         # publisher
@@ -27,7 +31,7 @@ class CircleTurtleNode(Node):
 
     def publish_cmd(self):
         msg = Twist()
-        if self.cur_point_index == len(self.points)-1:
+        if self.cur_point_index == self.points_length-1:
             msg.linear.x = 0.0
             msg.linear.y = 0.0
             self.publisher_.publish(msg)
@@ -50,7 +54,7 @@ class CircleTurtleNode(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    node = CircleTurtleNode()
+    node = SpiralTurtleNode()
     try:
         rclpy.spin(node)
     except:
