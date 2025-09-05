ce6cd446a02b75f5f9fd6f6545e9872d4f6d8ccb
EvanGupta <evangupta08@gmail.com>
2025-09-05 20:05:43 +0530

    Update setup.py

--- src/task2/turtle_draw/setup.py
+++ src/task2/turtle_draw/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'circle_drawer = turtle_draw.circle_drawer:main'
+            'square_drawer = turtle_draw.circle_drawer:main'
         ],
     },
 )
