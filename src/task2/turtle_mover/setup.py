e109cb90ccb287bf5a660aabf739b4d05810a2bc
Vishal Palli <f20240743@hyderabad.bits-pilani.ac.in>
2025-09-05 16:59:34 +0530

    Completed Tasks

---
+++ src/task2/turtle_mover/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = 'turtle_mover'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Turtle mover package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'circle_drawer = turtle_mover.circle_drawer:main',
+        ],
+    },
+)
